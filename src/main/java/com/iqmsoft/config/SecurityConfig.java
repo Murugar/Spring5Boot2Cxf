84519629e592ec25fbc93c1e14e27a3fb91be436
Muruga <davanon2014@gmail.com>
2021-10-16 11:44:46 +0530

    first

--- src/main/java/com/iqmsoft/config/SecurityConfig.java
+++ src/main/java/com/iqmsoft/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.iqmsoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,17 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
 
     }
+    
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception{
 
         //this configuration define that every request is authorized only to Admin roles and authenticated users.
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+        http.antMatcher("/**")
+                .authorizeRequests().antMatchers("/**").authenticated()
+                .anyRequest().hasRole("ADMIN")
+                
+                
+                //.hasRole("ADMIN")
+                
+                
 
                 //this configuration define an http basic authentication
                 .and().httpBasic()
